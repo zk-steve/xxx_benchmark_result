2d26934065abd9ee094bbc5d94c8cdbdf3c6ee7c
github-action-benchmark <github@users.noreply.github.com>
2024-12-31 07:12:52 +0000

    add Rust Benchmark (cargo) benchmark result for 4023a480abb5a515b80b2867a71c555335775b4d

---
+++ data.js
@@ -0,0 +1,67 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1735629172775,
+  "repoUrl": "https://github.com/zk-steve/rust_interview",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nguyen.thanh.son@outpost24.com",
+            "name": "Son Nguyen"
+          },
+          "committer": {
+            "email": "sonntuet1997@gmail.com",
+            "name": "Steve Nguyen",
+            "username": "zk-steve"
+          },
+          "distinct": true,
+          "id": "4023a480abb5a515b80b2867a71c555335775b4d",
+          "message": "fix CI",
+          "timestamp": "2024-12-31T14:11:09+07:00",
+          "tree_id": "12a728878e78d59eaf5e932bc856c817131b08ae",
+          "url": "https://github.com/zk-steve/rust_interview/commit/4023a480abb5a515b80b2867a71c555335775b4d"
+        },
+        "date": 1735629171616,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "forward_inplace/prime/2048",
+            "value": 92560,
+            "range": "± 160",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "forward_inplace/prime/4096",
+            "value": 199439,
+            "range": "± 280",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "forward_inplace/prime/8192",
+            "value": 428739,
+            "range": "± 381",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "forward_inplace/prime/16384",
+            "value": 913838,
+            "range": "± 771",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "forward_inplace/prime/32768",
+            "value": 1940253,
+            "range": "± 3699",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "forward_inplace/prime/65536",
+            "value": 4376138,
+            "range": "± 14345",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
